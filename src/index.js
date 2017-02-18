0c19b302440aeba29c13899bec84f84b4c6c4881
shpakkdv <shpak_kdv@mail.ru>
2017-02-18 22:24:30 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function sum() {
-  // your solution
+	var allSum = 0;
+	if (arguments.length === 0) {
+		return allSum;
+	} else {
+		for (let i = 0; i < arguments.length; i++) {
+			allSum += arguments[i];
+		}
+		return function nextSum() {
+			if (arguments.length === 0) {
+				let temp = allSum;
+				allSum = 0;
+				return temp;
+			} else {
+				for (let i = 0; i < arguments.length; i++) {
+					allSum += arguments[i];
+				}
+				return nextSum;
+			}
+		}
+	}
 }
